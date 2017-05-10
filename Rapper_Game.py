258d226fc5ad6ff757462bead94051124a17b86d
Angela Santurbano <angelasanturbano@Angelas-MacBook-Pro-2.local>
2017-05-09 18:41:04 -0700

    Second version

--- Rapper_Game.py
+++ Rapper_Game.py
@@ -1,5 +1,19 @@
 #Pseudo code for the Guess the Rapper terminal-based game
 
+# Creating a dictionary with all key-pair values for the lyrics and rapper
+rap_pair = {
+    "I remember syrup sandwiches and crime allowances": "kendrick lamar",
+    "Percocets, molly, Percocets": "future",
+    "I got hustle, though ambition flow inside my DNA": "kendrick lamar",
+    "Let the suicide doors up, I threw suicies on the tour bus": "kanye west",
+    "Man, I promise I'm so self-conscious, that's why you always see me with at least one of my watched": "kanye west",
+    "The lonely stoner seems to free his mind at night": "kid cudi",
+    "Shake it like a Polaroid picture": "outkast",
+    "Pretty woman, wassup? Is you here right now? You a stand-up or is you in your chair right now?": "nicki minaj",
+    "I'm so heartless, thoughtless, lawless and flawless, smallest, regardless": "nicki minaj",
+    "Tell the promoter we need more seats, we just sold out all the floor seats": "nicki minaj"
+        }          
+
 
 # - Greet user
 print #space
@@ -24,6 +38,11 @@ print
 #     - The game will count right guesses and let you know how close 
 # you are to becoming a rapper
 
+#FUNCTION FOR MENU
+#FUNCTON FOR INCREASING THE SCORE
+#FUNCTION FOR PLAYING THE GAME == starts at zero and then increases
+# Making code easier to read, simple and utilizing what we learned during class
+
 print "In this game, I'll ask you to guess the rapper behind the lyrics"
 print
 print "\t* I'll let you know when you've guessed right or wrong after each guess"
@@ -31,7 +50,8 @@ print
 print "\t* At the end, I'll let you know how close you are to becoming a rapper"
 
 print 
-print "Are you ready to start playing? (type exit if you would like to end the game"
+print "Are you ready to start playing? "
+print "(type exit at any point if you would like to end the game)"
 print
 
 ready_to_play = raw_input(">>> ")
@@ -45,20 +65,15 @@ print
 #NEED A BREAK STATEMENT TO EXIT
 #ADD MENU TO START GAME AGAIN 
 
-if ready_to_play.isdigit():
-    print "Sorry, I don't know what that means"
-
-else:
-    if ready_to_play.lower() == "yes":
-        print "Go"
+while True:
+    
+    if ready_to_play.lower() == "exit":
+        break
+    
     elif ready_to_play.lower() == "no":
         print "Okay, I understand. No pressure, just come back when you're ready"
-    else:
-        print "Okay let me know"
-
-# Game starts
-# - ~100 rap lyrics saved in the database, different random rap lyric
-# pulled up each time
+        break
+    #INFINITE LOOP
 
 # - Print random rap lyric
 # - Prompt for user inpur
@@ -66,11 +81,48 @@ else:
 #     If incorrect, print other message, count as 0
 #     Add to total score
 #     Return
-# - Repeat 14 more times
+# - Repeat 9 more times
 
-# After 15 guesses,
-# - If 0-5 out of 15 guesses right, print message
-# - If 6-10 out of 15 guesses right, print message
-# - If 11-15 out of 15 guesses right, print message
+    score = 0
 
-# Prompt user if they want to play again
+    elif ready_to_play.lower() == "yes":
+           
+           # print random.sample(rap_pair, 1) and return random.sample(rap_pair())        
+
+            for key in rap_pair:
+                print key
+                guess = raw_input(">>> ")
+
+                if guess.lower() == rap_pair[key]:
+                    print "That's a win!"
+                    print #morespace
+
+                else:
+                    print "I'm afraid that's the wrong guess..."
+
+    else:
+        print "Sorry, I don't know what that means. Are you ready to play?"
+        ready_to_play = raw_input(">>> ")
+
+#Don't allow duplicates in the random /shuffle option - make a list 
+#to keep track of which lyric they've already seen
+
+
+#Try to make a game that you would personally like to play
+
+# Game starts
+# - ~100 rap lyrics saved in the database, different random rap lyric
+# pulled up each time
+# API from Rap Genius
+
+
+# After 10 guesses,
+# - If 0-5 out of 10 guesses right, print message
+# - If 6-7 out of 10 guesses right, print message
+# - If 8-10 out of 10 guesses right, print message
+
+#(Store high scores)
+
+# Prompt user if they want to play again - have a function to bring 
+#them back to the top or put the whole thing in a loop
+
